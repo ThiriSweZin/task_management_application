4a47e1441c2cdd130ab77c344231e7bc9f776a8d
Thiri Swezin <thiriswezin129@gmail.com>
2022-09-25 14:35:49 +0630

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /**
  * Main Application
  */
-// var app = require('./dist/app');
-// module.exports = app.default;
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+var app = require('./dist/app');
+module.exports = app.default;
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3060;
+// }
+// app.listen(port);
