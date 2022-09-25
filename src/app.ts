1319910ced084574223350453737241054045b42
Thiri Swezin <thiriswezin129@gmail.com>
2022-09-25 12:35:46 +0630

    first Commit

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,9 @@ import { ExpressApplication } from "./lib/express-application";
 import * as passport from "./config/passport-config";
 import * as RestApi from "./lib/restapi";
 import * as schedule from "node-schedule";
-// import * as schedule from "node-schedule";
 import config from "../data/config.json";
 import * as filerouter from "./routes/file";
-// import { Auth } from "./lib/auth";
 import { JwtAuth } from "./lib/jwt";
-import { OrderView } from "./models/orderview";
-import { CustomerView } from "./models/customerview";
 import { months } from "moment";
 
 
@@ -58,44 +54,16 @@ class MainApp extends ExpressApplication {
     config.restapi.modelBasePath = path.join(__dirname, "models");
     this.use("/api", RestApi.init(config.restapi, (err, api) => {
       if (api) {
-        api.applyModel("notificationview", "regionview", "feedbackview", "cargateview", "bankview", "authview", "adsview", "categoryview", "productview", "subcategoryview", "customerview", "orderview", "dashboardview", "companyview", "forceupdateview", "reportview");
+        api.applyModel("");
       }
     }));
 
     this.use("/file", filerouter.default.init());
-
-    var rule = new schedule.RecurrenceRule();
-    rule.dayOfWeek = [0, new schedule.Range(1, 6)];
-    rule.hour = 17;
-    rule.minute = 0;
-    schedule.scheduleJob(rule, this.updateCustomerActive);
-
-    // schedule.scheduleJob({ second: 10 }, this.updateCustomerActive);
-    // schedule.scheduleJob({ hour: 5, minute: 30 }, this.callsendWarningNoti);
-    // schedule.scheduleJob({ hour: 8, minute: 30 }, this.callsendCancelNoti);
   }
 
   public onUseRouter(app: express.Express): void {
     this.loadRouters("./routes/pages");
   }
-
-  private updateCustomerActive() {
-    console.log("updated customer status active ");
-    const customerview = new CustomerView();
-    customerview.updateCustomerActive();
-  }
-
-  // private callsendWarningNoti() {
-  //   console.log("warning noti");
-  //   const orderview = new OrderView();
-  //   // orderview.sendWarningNoti();
-  // }
-
-  // private callsendCancelNoti() {
-  //   console.log("cancel noti");
-  //   const orderview = new OrderView();
-  //   orderview.sendCancelNoti();
-  // }
 }
 
 const app = new MainApp();
--- src/lib/permission.ts
+++ src/lib/permission.ts
@@ -5,103 +5,10 @@ export class Permission {
 
   constructor() { }
 
-  // 0: dashboard_Menu | 1: manage_user_Menu | 2: general_Menu | 3: main_Menu | 4: categories_Menu
   public static onLoad(req: express.Request, res: express.Response, next: express.NextFunction): void {
-    // console.log("permission load");
-    const dashboard: any[] = [], manage_user: any[] = [], general: any[] = [], info: any[] = [], customer: any[] = [], order: any[] = [], report: any[] = [];
-    const seperator: any = new RegExp(["\\\/", "\\\?"].join("|"), "g");
-    const proCome: string = req.url.replace("/", "").split(seperator)[0];
-    console.log(proCome);
-    let proGive: any;
+    console.log("permission load");
     if (req.isAuthenticated()) {
-      if (req.user.id == 0) {
-        console.log("user -1 ");
-        (async () => {
-          await RestApi.getDb("program").select()
-            .then((result) => {
-              for (const i in result) {
-                result[i].read = 1;
-                result[i].write = 1;
-                result[i].delete = 1;
-                switch (result[i].menu_no) {
-                  case 0: dashboard.push(result[i]); break;
-                  case 1: general.push(result[i]); break;
-                  case 2: manage_user.push(result[i]); break;
-                  case 3: info.push(result[i]); break;
-                  case 4: customer.push(result[i]); break;
-                  case 5: order.push(result[i]); break;
-                  case 6: report.push(result[i]); break;
-                  default: break;
-                }
-              }
-              proGive = result.find((pp: any) => {
-                return pp.program == proCome;
-              });
-            })
-            .catch((err) => {
-              console.log(`${err}`);
-            });
-          // console.log("-1 == ProGive : ", proGive);
-          res.locals.permission = {
-            dashboard: dashboard,
-            manage: manage_user,
-            general: general,
-            info: info,
-            customer: customer,
-            order: order,
-            report: report,
-            access: proGive.read + "," + proGive.write + "," + proGive.delete
-          };
-          await next();
-        })();
-      } else {
-        (async () => {
-          await RestApi.getDb("user").columns("roleid").where({ id: req.user.id }).select()
-            .then((result) => {
-              return RestApi.getDb().columns("program.*", "user_role_item.read", "user_role_item.write", "user_role_item.delete")
-                .select()
-                .from("program")
-                .leftJoin("user_role_item", "user_role_item.programid", "program.id")
-                .where({ "user_role_item.roleid": result[0].roleid });
-            })
-            .then((result) => {
-              // console.log("result ", result);
-              for (const i in result) {
-                if (!(result[i].read == 0 && result[i].write == 0 && result[i].delete == 0)) {
-                  switch (result[i].menu_no) {
-                    case 0: dashboard.push(result[i]); break;
-                    case 1: general.push(result[i]); break;
-                    case 2: manage_user.push(result[i]); break;
-                    case 3: info.push(result[i]); break;
-                    case 4: customer.push(result[i]); break;
-                    case 5: order.push(result[i]); break;
-                    case 6: report.push(result[i]); break;
-                    default: break;
-                  }
-                  proGive = result.find((pp: any) => {
-                    return pp.program == proCome;
-                  });
-                }
-              }
-              // console.log("general ", general);
-              // console.log(req.user.id + "ProGive : ", proGive);
-            })
-            .catch((err) => {
-              console.log(`${err}`);
-            });
-          res.locals.permission = {
-            dashboard: dashboard,
-            manage: manage_user,
-            general: general,
-            info: info,
-            customer: customer,
-            order: order,
-            report: report,
-            access: proGive.read + "," + proGive.write + "," + proGive.delete
-          };
-          await next();
-        })();
-      }
+      res.redirect(`/login?url=${req.url}`);
     } else {
       res.redirect(`/login?url=${req.url}`);
     }
