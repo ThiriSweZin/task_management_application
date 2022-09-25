e8a5ea633ebe8387083931bad1dae047c9ceef58
Thiri Swezin <thiriswezin129@gmail.com>
2022-09-25 13:20:03 +0630

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 /**
  * Main Application
  */
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3060 
+
+// static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
 var app = require('./dist/app');
 module.exports = app.default;
