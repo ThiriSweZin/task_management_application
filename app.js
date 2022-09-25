ccddcd4d3cb0cb0381d8f8d3837434c79c5ee383
Thiri Swezin <thiriswezin129@gmail.com>
2022-09-25 13:52:37 +0630

    My first commit

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 /**
  * Main Application
  */
-var app = require('./dist/app');
-module.exports = app.default;
+// var app = require('./dist/app');
+// module.exports = app.default;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
